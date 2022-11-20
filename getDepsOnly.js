2e57abfbe4d496a8ca75b017c6462a0530e3caf2
grau <A.Voss@gmx.de>
2022-11-20 21:20:46 +0100

    Change deps to commonJs

--- getDepsOnly.js
+++ getDepsOnly.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-import fs from 'fs';
+const fs = require('fs');
 
 const deps = JSON.parse(fs.readync('package.json', 'utf-8'));
 console.log(Object.keys(deps.dependencies).join(' '));
