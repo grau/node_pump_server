79819c8339da302c4800cd5d51b3d3b385bc3917
grau <A.Voss@gmx.de>
2022-11-20 21:21:46 +0100

    readSync ==> readFileSync

--- getDepsOnly.js
+++ getDepsOnly.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const deps = JSON.parse(fs.readync('package.json', 'utf-8'));
+const deps = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
 console.log(Object.keys(deps.dependencies).join(' '));
