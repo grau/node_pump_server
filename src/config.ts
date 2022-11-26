fff63591f7d5e7fc1e2b41885c67c80aaf108ddc
grau <A.Voss@gmx.de>
2022-11-26 16:13:57 +0100

    Add __dirname to data path

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,12 @@
  * @file Config options
  */
 
-/** Location of data storage */
-export const dataStorage = './data';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+ 
+
+/** Location of data storage */
+export const dataStorage = join(__dirname, 'data');
